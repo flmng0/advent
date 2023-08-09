5656f392d53a7e38a0dd399f30517a7889c84672
Timothy Davis <tmthydvs@gmail.com>
2023-08-09 16:05:42 +0930

    Add script to download input

---
+++ get_input.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+OUT_DIR="inputs"
+[ -d $OUT_DIR ] || mkdir $OUT_DIR
+
+DAY=$1
+
+YEAR=$2
+[ -z "$YEAR" ] && YEAR="2022"
+
+if [ -z "$ADVENT_SESSION" ]; then
+	echo "Make sure to set ADVENT_SECRET"
+	exit 1
+fi
+
+curl https://adventofcode.com/"$YEAR"/day/"$DAY"/input \
+	--silent \
+	--cookie session=$ADVENT_SESSION \
+	--output "$OUT_DIR/day$DAY.txt"
