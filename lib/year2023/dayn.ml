62b0a206d0f01322a7929b7563f0999ebd1b25cd
Timothy Davis <tmthydvs@gmail.com>
2023-12-10 06:08:26 +0930

    Add day template

---
+++ lib/year2023/dayn.ml
@@ -0,0 +1,21 @@
+(*
+
+open Util
+open Base
+
+let day = N
+
+let part_a _input = ""
+
+let part_b _input = ""
+
+let%test_module "day N" =
+  (module struct
+    let input = {||}
+
+    let%test_unit "part a" = [%test_result:string] (part_a input) ~expect:""
+
+    let%test_unit "part b" = [%test_result:string] (part_b input) ~expect:""
+  end)
+
+*)
