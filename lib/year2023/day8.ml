ab3756e81f06f83e1891236475701eae5b54fccd
Timothy Davis <tmthydvs@gmail.com>
2023-12-12 09:02:49 +0930

    Day 8 B

--- lib/year2023/day8.ml
+++ lib/year2023/day8.ml
@@ -64,7 +64,32 @@ let part_a input =
 
   Int.to_string steps
 
-let part_b _input = ""
+let part_b input =
+  let map = DMap.of_string input in
+  let dirs = DMap.dir_seq map in
+
+  let rec walk steps nodes dirs =
+    match Sequence.next dirs with
+    | Some (dir, dirs) ->
+        let node_dir = DMap.(match dir with Left -> fst | Right -> snd) in
+        let next_nodes = List.map ~f:node_dir nodes in
+
+        if List.for_all next_nodes ~f:(String.is_suffix ~suffix:"Z") then steps + 1
+        else
+          let nodes = List.map ~f:(Map.find_exn map.nodes) next_nodes in
+          walk (steps + 1) nodes dirs
+    | None -> invalid_arg "ran out of dirs in an infinite sequence??"
+  in
+
+  let nodes =
+    Map.to_alist map.nodes
+    |> List.filter_map ~f:(fun (name, node) ->
+           if String.is_suffix ~suffix:"A" name then Some node else None)
+  in
+
+  let steps = walk 0 nodes dirs in
+
+  Int.to_string steps
 
 let%test_module "day 8" =
   (module struct
@@ -92,5 +117,19 @@ ZZZ = (ZZZ, ZZZ)
 |})
         ~expect:"6"
 
-    let%test_unit "part b" = [%test_result: string] (part_b input) ~expect:""
+    let%test_unit "part b" =
+      [%test_result: string]
+        (part_b
+           {|LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+|})
+        ~expect:"6"
   end)
