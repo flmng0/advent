b934c3a6da87c112f125635652d2da44cb6b0ab2
Timothy Davis <tmthydvs@gmail.com>
2023-12-15 16:25:33 +0930

    Day 5 b actually works! Thanks Miguel

--- lib/year2023/day5.ml
+++ lib/year2023/day5.ml
@@ -1,23 +1,33 @@
 open Util
 open Base
 
+(* TODO: Get rid of Int64 *)
 module Mapping = struct
-  type t = { routes : route list } [@@deriving show]
+  open Int64
 
-  and route = { dst_start : int; src_start : int; length : int }
-  [@@deriving show]
+  type t = { routes : route list } [@@deriving show]
+  and route = { dst_start : int64; src_start : int64; length : int64 }
 
   let route_range r = (r.src_start, r.src_start + r.length)
   let route_contains s r = s >= r.src_start && s < r.src_start + r.length
 
+  let show_route r =
+    let start, stop = route_range r in
+    Printf.sprintf "%i .. %i" (to_int_exn start) (to_int_exn stop)
+
   let route_of_string s =
     match read_nums s with
-    | [ ds; ss; l ] -> Some { dst_start = ds; src_start = ss; length = l }
+    | [ ds; ss; l ] ->
+        Some { dst_start = of_int ds; src_start = of_int ss; length = of_int l }
     | _ -> None
 
   let of_string s =
     let route_strings = s |> String.split ~on:'\n' |> List.tl_exn in
-    let routes = route_strings |> List.filter_map ~f:route_of_string in
+    let routes =
+      route_strings
+      |> List.filter_map ~f:route_of_string
+      |> List.sort ~compare:(fun a b -> compare a.src_start b.src_start)
+    in
 
     { routes }
 
@@ -34,30 +44,37 @@ module Mapping = struct
       | route :: rest -> (
           let rfirst, rlast = route_range route in
 
-          if rfirst > slast || rlast < sfirst then loop acc srange rest (i + 1)
+          if rfirst > slast || rlast < sfirst then loop acc srange rest (i + one)
           else
             match (route_contains sfirst route, route_contains slast route) with
             | true, true -> (srange, Some i) :: acc
+
             | false, false ->
+                (* Seed range extends beyond the whole route *)
                 let acc =
                   ((rfirst, rlast), Some i) :: ((sfirst, rfirst), None) :: acc
                 in
                 let srange = (rlast, slast) in
-                loop acc srange rest (i + 1)
+                loop acc srange rest (i + one)
+
             | true, false ->
                 let acc = ((sfirst, rlast), Some i) :: acc in
                 let srange = (rlast, slast) in
-                loop acc srange rest (i + 1)
+                loop acc srange rest (i + one)
+
             | false, true ->
                 ((rfirst, slast), Some i) :: ((sfirst, rfirst), None) :: acc)
-      | [] -> if slast - sfirst = 0 then acc else (srange, None) :: acc
+
+      | [] -> if slast - sfirst = zero then acc else (srange, None) :: acc
     in
 
-    loop [] range m.routes 0
+    loop [] range m.routes zero
 end
 
 let seeds_of_string s =
-  s |> String.chop_prefix_exn ~prefix:"seeds: " |> read_nums
+  s
+  |> String.chop_prefix_exn ~prefix:"seeds: "
+  |> read_nums |> List.map ~f:Int64.of_int
 
 let day = 5
 
@@ -71,9 +88,9 @@ let part_a input =
     List.map seeds ~f:(fun s -> List.fold ~init:s ~f:Mapping.transform mappings)
   in
 
-  let min = List.min_elt locations ~compare:Int.compare |> Option.value_exn in
+  let min = List.min_elt locations ~compare:Int64.compare |> Option.value_exn in
 
-  Int.to_string min
+  Int64.to_string min
 
 let part_b input =
   let chunks = chunks_of_string input in
@@ -81,44 +98,37 @@ let part_b input =
   let seed_ranges =
     List.hd_exn chunks |> seeds_of_string |> List.chunks_of ~length:2
     |> List.filter_map ~f:(function
-         | [ start; len ] -> Some (start, start + len)
+         | [ start; len ] -> Some Int64.(start, start + len)
          | _ -> None)
   in
+
   let mappings = List.tl_exn chunks |> List.map ~f:Mapping.of_string in
 
   let transformed =
-    List.fold mappings ~init:seed_ranges ~f:(fun ranges m ->
-        let ranges =
-          List.concat_map ~f:(fun r -> Mapping.split_range r m) ranges
-          |> List.map ~f:(fun (r, route) ->
-                 match route with
-                 | Some i ->
-                     let Mapping.{ src_start; dst_start; _ } =
-                       List.nth_exn Mapping.(m.routes) i
-                     in
-                     let offset = dst_start - src_start in
-                     let start, stop = r in
-                     (start + offset, stop + offset)
-                 | None -> r)
-        in
-        ranges)
+    List.fold mappings ~init:seed_ranges
+      ~f:
+        Int64.(
+          fun ranges m ->
+            List.concat_map ~f:(fun r -> Mapping.split_range r m) ranges
+            |> List.map ~f:(fun (r, route) ->
+                   match route with
+                   | Some i ->
+                       let Mapping.{ src_start; dst_start; _ } =
+                         List.nth_exn Mapping.(m.routes) (Int64.to_int_exn i)
+                       in
+                       let offset = dst_start - src_start in
+                       let start, stop = r in
+                       (start + offset, stop + offset)
+                   | None -> r))
   in
-
-  let sorted =
-    transformed |> List.sort ~compare:(fun (a, _) (b, _) -> Int.compare a b)
-  in
-  Stdio.printf "%s\n\n"
-    (sorted
-    |> List.map ~f:(fun (start, stop) -> Printf.sprintf "%i..%i" start stop)
-    |> String.concat ~sep:" ; ");
 
   let min =
     List.map transformed ~f:(fun (start, _stop) -> start)
-    |> List.min_elt ~compare:Int.compare
+    |> List.min_elt ~compare:Int64.compare
     |> Option.value_exn
   in
 
-  Int.to_string min
+  Int64.to_string min
 
 let%test_module "day 5" =
   (module struct
