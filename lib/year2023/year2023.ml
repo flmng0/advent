c5747f50bc683f664b9e2dbb8bf35325e73d8780
Timothy Davis <tmthydvs@gmail.com>
2023-12-19 02:54:28 +0930

    Day 14 A. Slow. Re-doing

--- lib/year2023/year2023.ml
+++ lib/year2023/year2023.ml
@@ -14,6 +14,7 @@ let () =
   Solver.add (module Day10);
   Solver.add (module Day11);
   Solver.add (module Day13);
+  Solver.add (module Day14);
   ()
 
 let solve ?(input = stdin) ?(b_side = false) day =
